d935a3ef7d045d83401a26076e267ca9206e9bee
frevega <frevega@publicisgroupe.net>
2022-06-11 01:49:43 -0500

    Assignment for week 1 CSE210

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -99,7 +99,8 @@ def has_winner(board):
 def check_draw(board):
     for i in range(3):
         for j in range(3):
-            if board[i][j] != "x" or board[i][j] != "o":
+            if board[i][j] != "x" and board[i][j] != "o":
+                print(f"current value: {board[i][j]}")
                 return False
     return True
 
